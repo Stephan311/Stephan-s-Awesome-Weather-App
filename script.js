10ac7cd0f2f1480efcaaf96cbf1e6e87acf8beda
stephan du Plooy <sdpphotography1@gmail.com>
2021-03-29 09:27:57 -0400

    Added More HTML to the different pages

---
+++ script.js
@@ -0,0 +1,9 @@
+var searchFormEl = document.querySelector("#user-form");
+
+function grabformSubmission() {
+    window.location.href = "C:\Users\Stephan\Pictures\Course\Stephan-s-Awesome-Weather-App\searchresults.html"
+}
+
+var inputValue = document.querySelector("city")
+
+
