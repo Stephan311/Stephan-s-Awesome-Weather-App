8d8e453e3627f7efd1f36a035d378c97b69f0670
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-04 17:13:15 -0400

    Added weather for current day forecast

--- script.js
+++ script.js
@@ -26,24 +26,37 @@ console.log(Foo);
 $.getJSON(Citystring1 + Citystring2 + Citystring3, function (data) {
         console.log(data);
 
-    var weathericon = "http://openweathermap.org/img/w/" + data.weather[1].icon + ".png";
-    //gets tempreture. math.floor takes decimal away from tempreture
-    var temp = Math.floor(data.main.temp);
-    //gets weather desecription
-    var weather = data.weather[1].list;
+    // var weathericon = "http://openweathermap.org/img/w/" + data.list.main[0].icon + ".png";
+    // //gets tempreture. math.floor takes decimal away from tempreture
+    // var temp = Math.floor(data.list.temp);
+    // //gets weather desecription
+    // var weather = data.list[0].main;
 
     //weather icon displays inside its Div
+    // $('.icon').attr('src', weathericon);
+    // $(".temp").append(temp);
+    // $(".weather").append(weather);
+
+    //5 day forecast gets appeneded into each div block
+    //variables for each (day 1 start)
+    //day 1
+    var date = data.list[0].dt_txt;
+    console.log(date);
+    var temp = data.list[0].main.temp;
+    console.log(temp);
+    var humid = data.list[0].main.humidity;
+    console.log(humid);
+    var weathericon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+    console.log(weathericon);
     $('.icon').attr('src', weathericon);
-    $(".temp").append(temp);
-    $(".weather").append(weather);
-    
+    $('.temp').append(temp);
+    $('.humidity').append(humid);
 
 
 
 });
 }
 
-//logs weather data from new API
 $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=Ottawa&units=metric&appid=321743875c786c01219980579e46cc93", function(Hi){
     console.log(Hi);
 })
